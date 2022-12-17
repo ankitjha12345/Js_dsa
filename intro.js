17f9b73fe4b57523da71b23a8426685215dd2e00
Ankit Kumar <76030662+ankitjha12345@users.noreply.github.com>
2022-12-17 23:50:59 +0530

    Create intro.js

---
+++ intro.js
@@ -0,0 +1,38 @@
+What is Data Structure
+  ==> Way to organize data so that we can use this efficienlty
+  Example 
+    [8,5,5,2,4,9,2]
+    [0,1,2,2,3,4,5]
+
+What is Algorithm
+  ==> Steps or a process to arrange data.
+
+
+Data Structrue
+  ==> Primitive Data Structure 
+      
+      => Integer
+      => Float
+      => Charcter
+      => Pointer
+ 
+ ==> Non-Primitive Data Structure   
+     
+     => Arrays
+     => Lists
+     
+        => Linear Lists
+        
+          => Stacks
+          => Queues
+
+        => Non-Linear Lists
+          
+          => Graphs
+          => Trees
+          
+     => Files
+                                                                       
+                                                                       
+                                                  
+                                               
